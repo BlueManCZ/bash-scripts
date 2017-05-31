9f41597dc7c450e084a92d015cc207a3c22183c1
Ivo Šmerek <ivo97@centrum.cz>
2017-05-31 18:06:56 +0200

    Added ram_full.sh

---
+++ ram_full.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ram_free=$(awk '/MemAvailable/ {print $2}' /proc/meminfo)
+ram_total=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
+
+ram_free=$(( ${ram_free#0} ))
+ram_total=$(( ${ram_total#0} ))
+
+ram_used=$(( $ram_total - $ram_free ))
+
+echo $(( 100 * $ram_used / $ram_total ))'%'
